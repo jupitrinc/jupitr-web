ec8a3b01fab475d6a9109fd4cd7722fe0211b994
Farzad Wahabzada <imfrzd@gmail.com>
2023-06-03 21:06:57 +0100

    tailwind bug fixed

--- ui-library/avatar/avatar-group/AvatarGroup.stories.ts
+++ ui-library/avatar/avatar-group/AvatarGroup.stories.ts
@@ -14,22 +14,22 @@ const data = [
   {
     image_url:
       "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=928&q=80",
-    size: 10,
+    size: 20,
   },
   {
     image_url:
       "https://plus.unsplash.com/premium_photo-1663100696872-43eea3bf307b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2232&q=80",
-    size: 10,
+    size: 20,
   },
   {
     name_initials: "Elon Musk",
-    size: 10,
+    size: 20,
   },
   {
-    size: 10,
+    size: 20,
   },
   {
-    size: 10,
+    size: 20,
   },
 ]
 
--- ui-library/avatar/avatar/Avatar.stories.ts
+++ ui-library/avatar/avatar/Avatar.stories.ts
@@ -15,19 +15,19 @@ export const Photo: Story = {
   args: {
     image_url:
       "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=928&q=80",
-    size: 10,
+    size: 20,
   },
 }
 
 export const NameInitials: Story = {
   args: {
     name_initials: "Elon Musk",
-    size: 10,
+    size: 20,
   },
 }
 
 export const Placeholder: Story = {
   args: {
-    size: 10,
+    size: 20,
   },
 }
