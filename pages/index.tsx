d5d65c88f284f3b91a6f8a7c6ab1f0b716240b4d
Farzad Wahabzada <imfrzd@gmail.com>
2023-06-04 13:21:29 +0100

    tailwind missing utilities added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import { ReposContextProvider } from "state/repos/ReposContext"
 import { DefaultLayout } from "layouts/DefaultLayout"
-import { Avatar } from "ui-library/avatar/avatar/Avatar"
-import { Button } from "ui-library/button/Button"
 
 export default function Home() {
   return (
--- ui-library/button/Button.styles.ts
+++ ui-library/button/Button.styles.ts
@@ -1,8 +1,7 @@
 export const buttonStyles = {
-  button:
-    "font-medium flex-1 space-x-2 focus:outline-none ease-in-out disabled:cursor-not-allowed disabled:text-opacity-50 disabled:hover:text-opacity-50",
+  button: "font-medium flex-1 space-x-2 focus:outline-none ease-in-out",
   icon_button:
-    "font-medium p-3 text-center inline-flex items-center focus:outline-none ease-in-out disabled:cursor-not-allowed disabled:text-opacity-50 disabled:hover:text-opacity-50",
+    "font-medium p-3 text-center inline-flex items-center focus:outline-none ease-in-out",
 
   variant: {
     text: "bg-opacity-0",
@@ -11,11 +10,11 @@ export const buttonStyles = {
   },
   type: {
     default:
-      "text-gray-500 hover:text-gray-600 ring-gray-900/10 hover:ring-gray-900/20 bg-gray-50 hover:bg-gray-100 active:bg-gray-200",
+      "text-gray-500 hover:text-gray-600 ring-gray-900/10 hover:ring-gray-900/20 bg-gray-50 hover:bg-gray-100 active:bg-gray-200 disabled:cursor-not-allowed disabled:text-opacity-50 disabled:hover:text-opacity-50 disabled:hover:text-gray-500 disabled:hover:bg-gray-50 disabled:active:bg-gray-50",
     important:
-      "text-blue-500 hover:text-blue-600 ring-blue-900/10 hover:ring-blue-900/20 bg-blue-50 hover:bg-blue-100 active:bg-blue-200",
+      "text-blue-500 hover:text-blue-600 ring-blue-900/10 hover:ring-blue-900/20 bg-blue-50 hover:bg-blue-100 active:bg-blue-200 disabled:cursor-not-allowed disabled:text-opacity-50 disabled:hover:text-opacity-50 disabled:hover:text-blue-500 disabled:hover:bg-blue-50 disabled:active:bg-blue-50",
     dangerous:
-      "text-red-500 hover:text-red-600 ring-red-900/10 hover:ring-red-900/20 bg-red-50 hover:bg-red-100 active:bg-red-200",
+      "text-red-500 hover:text-red-600 ring-red-900/10 hover:ring-red-900/20 bg-red-50 hover:bg-red-100 active:bg-red-200 disabled:cursor-not-allowed disabled:text-opacity-50 disabled:hover:text-opacity-50 disabled:hover:text-red-500 disabled:hover:bg-red-50 disabled:active:bg-red-50",
   },
   rounded: {
     xl: "rounded-xl",
