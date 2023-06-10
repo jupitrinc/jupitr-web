856ae2360a4bfb5d18a5dadb73c38e3fb8b495a8
Ismael Teixeira <ismaelteixeira6@gmail.com>
2023-06-10 22:44:58 +0100

    stepper element (#4)
    Co-authored-by: Ismael Teixeira <ismael@MBP-de-Ismael.Home>

---
+++ ui-library/stepper/Stepper.styles..ts
@@ -0,0 +1,9 @@
+import { themeStyles } from "ui-library/_theme/Theme.styles"
+
+export const stepperStyles = {
+  container: "relative border-l border-gray-200",
+  item: "mb-10 ml-4 flex items-center",
+  item_container:
+    "absolute flex items-center justify-center w-8 h-8 rounded-full -left-4",
+  name: `ml-2`,
+}
