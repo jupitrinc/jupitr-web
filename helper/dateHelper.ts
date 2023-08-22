1e21b54a0ec7d53425b83a198b2e0fe1aa0a4e73
Faz <imfrzd@gmail.com>
2023-08-22 18:57:59 +0100

    clean up (#83)

--- helper/dateHelper.ts
+++ helper/dateHelper.ts
@@ -1,11 +1,6 @@
-import moment from "moment"
-
 export const dateHelper = {
   currentYear: () => {
     const now = new Date()
     return now.getFullYear()
-  },
-  prettyDate: (timestamp: string) => {
-    return moment(new Date(timestamp)).format("LL")
   },
 }
