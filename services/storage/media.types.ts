b5f28588626690e3021415950d44d188878bc4f2
Rafael Magalhaes <rafaelmagalhaes93@gmail.com>
2023-08-24 09:55:09 +0100

    JUP-264: update media types, added new function to upload applicant v… (#89)

--- services/storage/media.types.ts
+++ services/storage/media.types.ts
@@ -5,16 +5,22 @@ export interface StoragePayload {
 export interface MediaPayload extends StoragePayload {
   file: File
 }
+export interface JobMediaPayload {
+  file: File
+  company_id: string
+  user_id: string
+  job_id
+}
 
 export interface DownloadMediaPayload extends StoragePayload {}
 
 export type AvailableBucketsTypes =
-  | StorageBucketsEnum.avatars
   | StorageBucketsEnum.images
+  | StorageBucketsEnum.videos
 
 export enum StorageBucketsEnum {
-  avatars = "avatars",
   images = "images",
+  videos = "videos",
 }
 
 export const STORAGE_DOMAIN = `https://cgbrcxjbovzwarqujqoq.supabase.co/storage/v1/object/public`
