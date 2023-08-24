44a991d44fe22fd2a3768415de7e1123ee677ffc
Ismael Teixeira <ismael@MBP-de-Ismael.Home>
2023-08-24 23:53:36 +0100

    removing console logs

--- state/user/useUserAction.ts
+++ state/user/useUserAction.ts
@@ -127,13 +127,11 @@ export function useUserAction() {
   }
 
   const requestEmailUpdate = async (email: string) => {
-    console.log(email)
     dispatch({
       type: UserActionEnum.REQUEST_EMAIL_UPDATE_BEGIN,
     })
 
     const { error } = await changeEmailService(email)
-    console.log(error)
     if (error) {
       dispatch({
         type: UserActionEnum.REQUEST_EMAIL_UPDATE_FAILURE,
