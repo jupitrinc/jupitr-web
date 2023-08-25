10247ce38a371ddd9759ad6504161212088f6550
Farzad Wahabzada <imfrzd@gmail.com>
2023-08-25 21:35:04 +0100

    add video

--- components/company/job/job-edit/Videos.tsx
+++ components/company/job/job-edit/Videos.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Plus } from "lucide-react"
 import { VideoPlayer } from "ui-library/video/video-player/VideoPlayer"
 import { Text } from "ui-library/text/Text"
@@ -10,6 +10,11 @@ import { useCompanyJobState } from "state/company_job/useCompanyJobState"
 import InviteTeam from "components/company/member/InviteTeam"
 import { IJobVideo } from "state/company_job/companyJob.types"
 import { useCompanyJobAction } from "state/company_job/useCompanyJobAction"
+import { Modal } from "ui-library/modal/Modal"
+import { useNotification } from "helper/hooks/useNotification"
+import { VideoRecorder } from "ui-library/video/video-recorder/VideoRecorder"
+import { static_data_job } from "data/job"
+import { Button } from "ui-library/button/Button"
 
 export const Videos = () => {
   const { videos } = useCompanyJobState()
@@ -28,9 +33,12 @@ export const Videos = () => {
 }
 
 const AddVideos = () => {
+  const { notification, showNotification, hideNotification } = useNotification()
+  const [videoFile, setVideoFile] = useState<File | null>(null)
+
   return (
     <div className="grid grid-cols-2 gap-5 justify-center">
-      <Card type="linked">
+      <Card type="linked" onClick={showNotification}>
         <div className="flex flex-col gap-1 justify-center items-center self-center">
           <Plus className="h-5 w-5 text-gray-600" />
           <Text as="span" size="base">
@@ -38,6 +46,22 @@ const AddVideos = () => {
           </Text>
         </div>
       </Card>
+
+      <Modal open={notification} onClose={hideNotification}>
+        <div className="flex flex-col gap-5">
+          <Text as="span" size="xl">
+            Add video
+          </Text>
+
+          <VideoRecorder
+            duration={Number(static_data_job.video_duration[0])}
+            recordLabel="Start"
+            onChange={(videoFile) => setVideoFile(videoFile)}
+          />
+
+          {videoFile && <Button label="Add video" size="base" />}
+        </div>
+      </Modal>
 
       <InviteTeam title="Collaborate" />
     </div>
--- data/job.ts
+++ data/job.ts
@@ -1,5 +1,5 @@
 export const static_data_job = {
   skill_levels: ["Beginner", "Competent", "Expert"],
   work_models: ["Remote", "Office", "Hybrid"],
-  video_duration: ["15", "30", "45", "60"],
+  video_duration: ["15", "20", "30"],
 }
