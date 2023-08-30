7f587da842ae374b430c0c252636d7553862f1c1
Ismael Teixeira <ismael@MBP-de-Ismael.Home>
2023-08-30 22:55:20 +0100

    company job application with error handling

--- components/company/job/JobApplications.tsx
+++ components/company/job/JobApplications.tsx
@@ -6,14 +6,19 @@ import { useCompanyJobApplicationAction } from "state/company_job_application/us
 import { useCompanyJobApplicationState } from "state/company_job_application/useCompanyJobApplicationState"
 import ApplicationCard from "./job-applications/ApplicationCard"
 import { Loader } from "ui-library/loader/Loader"
+import { useNotification } from "helper/hooks/useNotification"
+import { stringHelper } from "helper/stringHelper"
+import { Toast } from "ui-library/toast/Toast"
 
 export const JobApplications = () => {
   const router = useRouter()
+  const { isEmpty } = stringHelper
   const { jobId } = router.query
   const { company_job_applications, loading, error } =
     useCompanyJobApplicationState()
   const { getAllApplications, clearApplications } =
     useCompanyJobApplicationAction()
+  const { notification, hideNotification } = useNotification(!isEmpty(error))
 
   useEffect(() => {
     if (jobId && !company_job_applications.id) {
@@ -65,6 +70,32 @@ export const JobApplications = () => {
       </div>
     )
   } else {
-    return null
+    return (
+      <div className="grid grid-cols-1 gap-5">
+        <div className="flex flex-col md:flex-row gap-5 justify-between items-center bg-gray-200 p-5 rounded-lg">
+          <div className="flex flex-col md:flex-row gap-5 items-center">
+            <Text as="span" size="xl" align="left">
+              {company_job_applications.title}
+            </Text>
+            <Button
+              label="View job"
+              variant="outlined"
+              size="xs"
+              onClick={viewJob}
+            />
+          </div>
+
+          <Text as="span" size="sm" align="right">
+            {`0 applications`}
+          </Text>
+        </div>
+
+        <Toast
+          onHide={hideNotification}
+          show={notification}
+          label={"There has been an error fetching the notifications."}
+        />
+      </div>
+    )
   }
 }
