6de668fc5d75d8a1fa024446f561c023f915ee1a
Farzad Wahabzada <faz@jupitr.tech>
2023-08-30 15:19:31 +0100

    styling bug

--- ui-library/content/card/Card.styles.ts
+++ ui-library/content/card/Card.styles.ts
@@ -1,12 +1,17 @@
 import { CardTypeEnum } from "./Card.types"
 
 export const cardBaseStyles = {
-  container: "flex flex-col justify-center",
+  container: "flex flex-col",
   active: "ring-gray-900/20 bg-gray-100",
   type: {
     [CardTypeEnum.static]: "ring-1 ring-gray-900/10 rounded-lg w-full p-3",
     [CardTypeEnum.linked]:
       "ring-1 ring-gray-900/10 rounded-lg w-full p-3 cursor-pointer overflow-hidden hover:ring-gray-900/20 hover:bg-gray-100",
     [CardTypeEnum.section]: "gap-10 bg-white rounded-lg p-5 sm:p-10",
+  },
+  justify: {
+    center: "justify-center",
+    start: "justify-start",
+    end: "justify-end",
   },
 }
