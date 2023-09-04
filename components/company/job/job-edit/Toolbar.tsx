3a42268f8c4104332ba688ecf485f88212506b37
Faz <faz@jupitr.tech>
2023-09-04 20:13:59 +0100

    company job preview (#120)

--- components/company/job/job-edit/Toolbar.tsx
+++ components/company/job/job-edit/Toolbar.tsx
@@ -5,6 +5,7 @@ import { useCompanyJobState } from "state/company_job/useCompanyJobState"
 import { Button } from "ui-library/button/Button"
 import { MenuBar } from "ui-library/menu/menu-bar/MenuBar"
 import { JobStatusEnum } from "state/company_job/companyJob.types"
+import { Eye } from "lucide-react"
 
 const Toolbar = () => {
   const router = useRouter()
@@ -14,8 +15,6 @@ const Toolbar = () => {
 
   const options = useMemo(
     () => [
-      // { name: "Preview", onClick: () => alert("") },
-      // { name: "Share", onClick: () => alert("") },
       {
         name: "Close",
         onClick: () => {
@@ -44,12 +43,22 @@ const Toolbar = () => {
     })
   }, [company_job])
 
+  const previewJob = useCallback(() => {
+    router.push(`/jobs/${company_job.id}`)
+  }, [company_job.id])
+
   return (
     <div className="basis-2/3 flex flex-row gap-2 items-center justify-end">
       <Button
         label={company_job.status === "open" ? "Pause" : "Publish"}
         variant={company_job.status === "open" ? "text" : "contained"}
         onClick={toggleStatus}
+      />
+      <Button
+        label="Preview"
+        variant="text"
+        icon={<Eye className="h-4 w-4" />}
+        onClick={previewJob}
       />
       <MenuBar options={options} max_number={0} variant="text" size="xs" />
     </div>
--- components/talent/job/JobView.tsx
+++ components/talent/job/JobView.tsx
@@ -2,6 +2,8 @@ import React, { useEffect } from "react"
 import { useTalentJobState } from "state/talent_job/useTalentJobState"
 import { useRouter } from "next/router"
 import { useTalentJobAction } from "state/talent_job/useTalentJobAction"
+import { useUserState } from "state/user/useUserState"
+import { AccountTypeEnum } from "state/user/user.types"
 import SkillList from "./job-view/SkillList"
 import Videos from "./job-view/Videos"
 import TitleBar from "./job-view/TitleBar"
@@ -13,6 +15,7 @@ import NoMatchFound from "ui-library/content/no-match-found/NoMatchFound"
 const JobView = () => {
   const router = useRouter()
   const { jobId } = router.query
+  const { user } = useUserState()
 
   const { talent_job, loading, error } = useTalentJobState()
   const { getJob, clearJob } = useTalentJobAction()
@@ -46,8 +49,14 @@ const JobView = () => {
     return (
       <NoMatchFound
         message="Job no longer available"
-        link="/jobs"
-        label="Find similar jobs"
+        link={
+          user.account_type === AccountTypeEnum.talent ? "/jobs" : "/c/jobs"
+        }
+        label={
+          user.account_type === AccountTypeEnum.talent
+            ? "Find similar jobs"
+            : "Go back"
+        }
       />
     )
   }
