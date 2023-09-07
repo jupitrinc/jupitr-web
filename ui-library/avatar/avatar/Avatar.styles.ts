ad57bc204fc8d6652cf7ba68f7c89b28ebf97030
Faz <faz@jupitr.tech>
2023-09-07 17:52:16 +0100

    bug fix (#126)

--- ui-library/avatar/avatar/Avatar.styles.ts
+++ ui-library/avatar/avatar/Avatar.styles.ts
@@ -1,8 +1,8 @@
 import { themeStyles } from "ui-library/_theme/Theme.styles"
 
 export const avatarStyles = {
-  container: `inline-flex overflow-hidden justify-center items-center ${themeStyles.backgroundColor.standard[100]} rounded-full ${themeStyles.textColor.standard[600]} border-2 border-white`,
+  container: `inline-flex overflow-hidden justify-center items-center ${themeStyles.backgroundColor.standard[100]} rounded-full ${themeStyles.textColor.standard[600]} ring-2 ring-white`,
   icon: "text-gray-400 h-2/4",
-  image: "object-cover",
+  image: "object-cover rounded-full ring-2 ring-white",
   size: (size: number) => `w-${size} h-${size}`,
 }
