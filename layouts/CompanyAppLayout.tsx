6a7078901226283f1a9945b8c807887d69ca9d08
Faz <faz@jupitr.tech>
2023-09-13 01:11:45 +0100

    touches (#132)

--- layouts/CompanyAppLayout.tsx
+++ layouts/CompanyAppLayout.tsx
@@ -1,10 +1,10 @@
-import Head from "next/head"
 import { Navbar } from "./components/Navbar"
 import { Footer } from "./components/Footer"
 import { useUserState } from "state/user/useUserState"
 import { AccountTypeEnum } from "state/user/user.types"
 import { usePersistedUser } from "components/user/sign-in/usePersistedUser"
 import PageNotFound from "./components/PageNotFound"
+import PageHead from "./components/PageHead"
 
 export const CompanyAppLayout = ({ children }) => {
   const { user } = useUserState()
@@ -13,31 +13,7 @@ export const CompanyAppLayout = ({ children }) => {
   if (user.account_type === AccountTypeEnum.company) {
     return (
       <>
-        <Head>
-          <title>jupitr</title>
-          <meta name="description" content="" />
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/site.webmanifest" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-          <meta name="msapplication-TileColor" content="#da532c" />
-          <meta name="theme-color" content="#ffffff" />
-        </Head>
+        <PageHead />
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 my-10">
           <header className="space-y-2">
             <Navbar />
