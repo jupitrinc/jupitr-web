ffd6e62ecd72262232f0c1a4c82df308ef8f5ff6
Ismael Teixeira <ismael@MBP-de-Ismael.Home>
2023-09-22 22:18:04 +0100

    applying final comments from faz

--- components/user/account-settings/AccountNotifications.tsx
+++ components/user/account-settings/AccountNotifications.tsx
@@ -1,69 +1,15 @@
-import React, { useState } from "react"
+import React from "react"
 import { useUserState } from "state/user/useUserState"
-import { Toggle } from "ui-library/form/toggle/Toggle"
-import { Tabs } from "ui-library/menu/tabs/Tabs"
-import { Text } from "ui-library/text/Text"
+import TalentNotifications from "./account-notifications/TalentNotifications"
+import CompanyNotifications from "./account-notifications/CompanyNotifications"
 
 const AccountNotifications = () => {
-  const intervals = ["Daily", "Weekly", "Never"]
-  const [interval, setInterval] = useState(0)
-  const [isEmailActive, setIsEmailActive] = useState(true)
   const { user } = useUserState()
 
   if (user.account_type === "talent") {
-    return (
-      <div className="flex flex-col gap-2">
-        <div className="w-full flex flex-row items-center justify-between gap-3">
-          <Text as="span" size="base">
-            Job Recommendations
-          </Text>
-          <Toggle
-            size="base"
-            checked={isEmailActive}
-            onChange={() => setIsEmailActive((prev) => !prev)}
-          />
-        </div>
-
-        <div className="w-full flex flex-row items-center justify-between gap-3">
-          <Text as="span" size="base">
-            Notifications sent each
-          </Text>
-          <Tabs
-            size="base"
-            items={intervals}
-            active_tab={interval}
-            onChange={(index) => setInterval(index)}
-          />
-        </div>
-      </div>
-    )
+    return <TalentNotifications />
   } else {
-    return (
-      <div className="mt-4 flex flex-col gap-2">
-        <div className="w-full flex flex-row items-center justify-between gap-3">
-          <Text as="span" size="base">
-            Job applications
-          </Text>
-          <Toggle
-            size="base"
-            checked={isEmailActive}
-            onChange={() => setIsEmailActive((prev) => !prev)}
-          />
-        </div>
-
-        <div className="w-full flex flex-row items-center justify-between gap-3">
-          <Text as="span" size="base">
-            Notifications sent each
-          </Text>
-          <Tabs
-            size="base"
-            items={intervals}
-            active_tab={interval}
-            onChange={(index) => setInterval(index)}
-          />
-        </div>
-      </div>
-    )
+    return <CompanyNotifications />
   }
 }
 
