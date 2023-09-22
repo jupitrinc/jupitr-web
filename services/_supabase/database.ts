9365e4f3761356237e7335bfeb0092344497248c
Rafael Magalhaes <rafaelmagalhaes93@gmail.com>
2023-09-22 20:21:27 +0100

    create notification service (#151)

--- services/_supabase/database.ts
+++ services/_supabase/database.ts
@@ -9,6 +9,52 @@ export type Json =
 export interface Database {
   public: {
     Tables: {
+      applications: {
+        Row: {
+          created_at: string
+          id: string
+          job_id: string | null
+          skills: Json | null
+          status: string | null
+          updated_at: string | null
+          user_id: string | null
+          video_url: string
+        }
+        Insert: {
+          created_at?: string
+          id?: string
+          job_id?: string | null
+          skills?: Json | null
+          status?: string | null
+          updated_at?: string | null
+          user_id?: string | null
+          video_url: string
+        }
+        Update: {
+          created_at?: string
+          id?: string
+          job_id?: string | null
+          skills?: Json | null
+          status?: string | null
+          updated_at?: string | null
+          user_id?: string | null
+          video_url?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "applications_job_id_fkey"
+            columns: ["job_id"]
+            referencedRelation: "jobs"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "applications_user_id_fkey"
+            columns: ["user_id"]
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
       company: {
         Row: {
           created_at: string | null
@@ -36,7 +82,7 @@ export interface Database {
         }
         Update: {
           created_at?: string | null
-          id: string
+          id?: string
           industry?: Json | null
           logo?: string | null
           mission?: string | null
@@ -50,40 +96,37 @@ export interface Database {
       }
       company_member_profile: {
         Row: {
-          company_id: string
+          company_id: string | null
           created_at: string | null
+          id: string
           job_title: string | null
-          roles: string
+          permission: Database["public"]["Enums"]["permission_type"] | null
           updated_at: string | null
-          user_id: string
+          user_id: string | null
         }
         Insert: {
-          company_id?: string
+          company_id?: string | null
           created_at?: string | null
+          id?: string
           job_title?: string | null
-          roles?: string
+          permission?: Database["public"]["Enums"]["permission_type"] | null
           updated_at?: string | null
-          user_id?: string
+          user_id?: string | null
         }
         Update: {
-          company_id?: string
+          company_id?: string | null
           created_at?: string | null
+          id?: string
           job_title?: string | null
-          roles?: string
+          permission?: Database["public"]["Enums"]["permission_type"] | null
           updated_at?: string | null
-          user_id?: string
+          user_id?: string | null
         }
         Relationships: [
           {
             foreignKeyName: "company_member_profile_company_id_fkey"
             columns: ["company_id"]
             referencedRelation: "company"
-            referencedColumns: ["id"]
-          },
-          {
-            foreignKeyName: "company_member_profile_roles_fkey"
-            columns: ["roles"]
-            referencedRelation: "company_member_roles"
             referencedColumns: ["id"]
           },
           {
@@ -94,82 +137,223 @@ export interface Database {
           }
         ]
       }
-      company_member_roles: {
+      company_videos: {
         Row: {
-          company_id: string
-          created_at: string | null
+          created_at: string
           id: string
-          permission: Database["public"]["Enums"]["permission_type"] | null
-          service_level:
-            | Database["public"]["Enums"]["service_level_type"]
-            | null
-          updated_at: string | null
-          user_id: string
+          job_id: string
+          user_id: string | null
+          video_url: string
         }
         Insert: {
-          company_id: string
-          created_at?: string | null
+          created_at?: string
           id?: string
-          permission?: Database["public"]["Enums"]["permission_type"] | null
-          service_level?:
-            | Database["public"]["Enums"]["service_level_type"]
-            | null
-          updated_at?: string | null
-          user_id: string
+          job_id: string
+          user_id?: string | null
+          video_url: string
         }
         Update: {
-          company_id?: string
-          created_at?: string | null
+          created_at?: string
           id?: string
-          permission?: Database["public"]["Enums"]["permission_type"] | null
-          service_level?:
-            | Database["public"]["Enums"]["service_level_type"]
-            | null
-          updated_at?: string | null
-          user_id?: string
+          job_id?: string
+          user_id?: string | null
+          video_url?: string
         }
         Relationships: [
           {
-            foreignKeyName: "company_member_roles_company_id_fkey"
-            columns: ["company_id"]
-            referencedRelation: "company"
+            foreignKeyName: "company_videos_job_id_fkey"
+            columns: ["job_id"]
+            referencedRelation: "jobs"
             referencedColumns: ["id"]
           },
           {
-            foreignKeyName: "company_member_roles_user_id_fkey"
+            foreignKeyName: "company_videos_user_id_fkey"
             columns: ["user_id"]
             referencedRelation: "users"
             referencedColumns: ["id"]
           }
         ]
       }
-      talent_profile: {
+      feedback: {
         Row: {
-          searching: boolean | null
-          jobs: Json | null
-          preferences: Json | null
+          created_at: string
+          id: number
+          message: string | null
+          rating: number | null
+          user_id: string | null
+        }
+        Insert: {
+          created_at?: string
+          id?: number
+          message?: string | null
+          rating?: number | null
+          user_id?: string | null
+        }
+        Update: {
+          created_at?: string
+          id?: number
+          message?: string | null
+          rating?: number | null
+          user_id?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "feedback_user_id_fkey"
+            columns: ["user_id"]
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      industry: {
+        Row: {
+          id: string
+          name: string | null
+        }
+        Insert: {
+          id?: string
+          name?: string | null
+        }
+        Update: {
+          id?: string
+          name?: string | null
+        }
+        Relationships: []
+      }
+      jobs: {
+        Row: {
+          application_video: Json | null
+          company_id: string | null
+          created_at: string
+          id: string
+          location: Json | null
+          salary: string | null
           skills: Json | null
-          socials: Json | null
+          status: string | null
+          title: string | null
+          updated_at: string | null
+          "visa_sponsorshi[": boolean | null
+          work_model: Json | null
+        }
+        Insert: {
+          application_video?: Json | null
+          company_id?: string | null
+          created_at?: string
+          id?: string
+          location?: Json | null
+          salary?: string | null
+          skills?: Json | null
+          status?: string | null
+          title?: string | null
+          updated_at?: string | null
+          "visa_sponsorshi["?: boolean | null
+          work_model?: Json | null
+        }
+        Update: {
+          application_video?: Json | null
+          company_id?: string | null
+          created_at?: string
+          id?: string
+          location?: Json | null
+          salary?: string | null
+          skills?: Json | null
+          status?: string | null
+          title?: string | null
+          updated_at?: string | null
+          "visa_sponsorshi["?: boolean | null
+          work_model?: Json | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "jobs_company_id_fkey"
+            columns: ["company_id"]
+            referencedRelation: "company"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      notifications: {
+        Row: {
+          active: boolean | null
+          alert: Database["public"]["Enums"]["alert_type"]
+          created_at: string
+          id: string
+          interval: Database["public"]["Enums"]["interval_type"] | null
+          type: Json | null
           updated_at: string | null
           user_id: string
         }
         Insert: {
-          searching?: boolean | null
-          jobs?: Json | null
-          preferences?: Json | null
-          skills?: Json | null
-          socials?: Json | null
+          active?: boolean | null
+          alert: Database["public"]["Enums"]["alert_type"]
+          created_at?: string
+          id?: string
+          interval?: Database["public"]["Enums"]["interval_type"] | null
+          type?: Json | null
           updated_at?: string | null
           user_id: string
         }
         Update: {
-          searching?: boolean | null
-          jobs?: Json | null
+          active?: boolean | null
+          alert?: Database["public"]["Enums"]["alert_type"]
+          created_at?: string
+          id?: string
+          interval?: Database["public"]["Enums"]["interval_type"] | null
+          type?: Json | null
+          updated_at?: string | null
+          user_id?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "notifications_user_id_fkey"
+            columns: ["user_id"]
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      skills: {
+        Row: {
+          id: string
+          name: string
+        }
+        Insert: {
+          id?: string
+          name: string
+        }
+        Update: {
+          id?: string
+          name?: string
+        }
+        Relationships: []
+      }
+      talent_profile: {
+        Row: {
+          id: string
+          preferences: Json | null
+          searching: boolean | null
+          skills: Json | null
+          socials: Json | null
+          updated_at: string | null
+          user_id: string | null
+        }
+        Insert: {
+          id?: string
           preferences?: Json | null
+          searching?: boolean | null
           skills?: Json | null
           socials?: Json | null
           updated_at?: string | null
-          user_id?: string
+          user_id?: string | null
+        }
+        Update: {
+          id?: string
+          preferences?: Json | null
+          searching?: boolean | null
+          skills?: Json | null
+          socials?: Json | null
+          updated_at?: string | null
+          user_id?: string | null
         }
         Relationships: [
           {
@@ -222,24 +406,57 @@ export interface Database {
       [_ in never]: never
     }
     Functions: {
-      handle_new_company: {
-        Args: {
-          p_name: string
-          p_year_founded: string
-          p_logo: string
-          p_website: string
-          p_size: number
-          p_mission: string
-          p_user_id: string
-          p_permission: Database["public"]["Enums"]["permission_type"]
-          p_service_level: Database["public"]["Enums"]["service_level_type"]
-          p_industry: Json
-        }
-        Returns: {
-          company_id: string
-          role_id: string
-        }[]
-      }
+      handle_new_company:
+        | {
+            Args: {
+              p_name: string
+              p_year_founded: string
+              p_logo: string
+              p_website: string
+              p_size: string
+              p_mission: string
+              p_user_id: string
+              p_permission: Database["public"]["Enums"]["permission_type"]
+              p_service_level: Database["public"]["Enums"]["service_level_type"]
+              p_industry: Json
+            }
+            Returns: {
+              company_id: string
+              role_id: string
+            }[]
+          }
+        | {
+            Args: {
+              p_name: string
+              p_year_founded: string
+              p_website: string
+              p_size: string
+              p_mission: string
+              p_user_id: string
+              p_permission: Database["public"]["Enums"]["permission_type"]
+              p_service_level: Database["public"]["Enums"]["service_level_type"]
+              p_industry: Json
+            }
+            Returns: {
+              company_id: string
+              role_id: string
+            }[]
+          }
+        | {
+            Args: {
+              p_name: string
+              p_year_founded: string
+              p_website: string
+              p_size: string
+              p_mission: string
+              p_user_id: string
+              p_permission: Database["public"]["Enums"]["permission_type"]
+              p_industry: Json
+            }
+            Returns: {
+              company_id: string
+            }[]
+          }
       handle_new_company_member: {
         Args: {
           p_company_id: string
@@ -254,6 +471,8 @@ export interface Database {
     }
     Enums: {
       account_types: "company" | "talent"
+      alert_type: "job_alert" | "collab_alert" | "applications_alert"
+      interval_type: "daily" | "weekly"
       permission_type: "read" | "write"
       service_level_type: "company" | "job" | "team"
     }
