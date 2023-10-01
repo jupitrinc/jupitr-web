d213e94a1df658f1efcd8b031a5abff46e9a2a3a
rafa <rafaelmagalhaes93@gmail.com>
2023-10-01 20:11:10 +0100

    update videoTitle make job title not required

--- components/talent/job/job-view/videos/VideoTitle.tsx
+++ components/talent/job/job-view/videos/VideoTitle.tsx
@@ -1,18 +1,20 @@
 import { Text } from "ui-library/text/Text"
 
-const VideoTitle: React.FC<{ name: string; title: string }> = ({
+const VideoTitle: React.FC<{ name: string; title?: string }> = ({
   name,
   title,
 }) => {
-  if (name && title) {
+  if (name) {
     return (
       <div className="flex flex-col mt-3">
         <Text as="span" size="lg">
           {name}
         </Text>
-        <Text as="span" size="sm">
-          {title}
-        </Text>
+        {title ? (
+          <Text as="span" size="sm">
+            {title}
+          </Text>
+        ) : null}
       </div>
     )
   } else {
