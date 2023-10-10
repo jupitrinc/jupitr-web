03e16f3b2f9dcb8e2afca1fc72ad35c787f11d38
Faz <faz@jupitr.tech>
2023-10-10 09:32:30 +0100

    homepage job link (#166)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,37 @@
 import { WebsiteLayout } from "layouts/WebsiteLayout"
 import { SignIn } from "components/user/sign-in/SignIn"
 import { useUserState } from "state/user/useUserState"
+import { Button } from "ui-library/button/Button"
+import { useRouter } from "next/router"
+import { AccountTypeEnum } from "state/user/user.types"
+import { useCallback } from "react"
 
 export default function Home() {
+  const router = useRouter()
   const { user } = useUserState()
-  return <WebsiteLayout>{!user.id && <SignIn />}</WebsiteLayout>
+
+  const goToDashboard = useCallback(() => {
+    router.push(
+      user.account_type === AccountTypeEnum.company ? "/c/jobs" : "/jobs"
+    )
+  }, [user])
+
+  return (
+    <WebsiteLayout>
+      {!user.id && <SignIn />}
+
+      {user.id && (
+        <div className="flex max-w-sm justify-center m-auto text-center">
+          <Button
+            label="Jobs"
+            size="xl"
+            variant="outlined"
+            onClick={goToDashboard}
+            full_width
+            arrow
+          />
+        </div>
+      )}
+    </WebsiteLayout>
+  )
 }
