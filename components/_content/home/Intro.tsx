d93004580efeeabc7f320f72a6541ed2f18db058
Faz <faz@jupitr.tech>
2023-10-12 21:18:39 +0100

    bug fix (#169)

--- components/_content/home/Intro.tsx
+++ components/_content/home/Intro.tsx
@@ -3,17 +3,28 @@ import { Card } from "ui-library/content/card/Card"
 import { Text } from "ui-library/text/Text"
 import { Button } from "ui-library/button/Button"
 import { VideoPlayer } from "ui-library/video/video-player/VideoPlayer"
+import { urlHelper } from "helper/urlHelper"
 import SkillCard from "ui-library/content/card/skill-card-progress-bar/SkillCard"
 
 const Intro = () => {
   const router = useRouter()
+
+  const video = {
+    url: "https://res.cloudinary.com/dyfg2jhz8/video/upload/f_auto:video,q_auto/v1/production/company/d75f8ef2-40d4-4b4b-9047-c1b73c55b20d/jobs/d3ba1cf3-0a8d-4c09-9e8c-32eb967613f4/4b61daac-c490-4f3c-9f15-8e42cb527476-1695797858118/qukcglt0ukmuaadrjg59",
+    poster:
+      "production/company/d75f8ef2-40d4-4b4b-9047-c1b73c55b20d/jobs/d3ba1cf3-0a8d-4c09-9e8c-32eb967613f4/4b61daac-c490-4f3c-9f15-8e42cb527476-1695797858118/qukcglt0ukmuaadrjg59",
+  }
+
   return (
     <div className="flex flex-col sm:flex-row gap-5">
       <div className="basis-2/3">
         <Card type="section">
           <div className="flex flex-col gap-10">
             <Header title="Join" subtitle="A-player tech teams" />
-            <VideoPlayer src="https://res.cloudinary.com/dyfg2jhz8/video/upload/f_auto:video,q_auto/v1/production/company/d75f8ef2-40d4-4b4b-9047-c1b73c55b20d/jobs/d3ba1cf3-0a8d-4c09-9e8c-32eb967613f4/4b61daac-c490-4f3c-9f15-8e42cb527476-1695797858118/qukcglt0ukmuaadrjg59" />
+            <VideoPlayer
+              src={video.url}
+              poster={urlHelper.videoPosterUrl(video.poster)}
+            />
             <div className="flex justify-center">
               <Button
                 label="See job"
