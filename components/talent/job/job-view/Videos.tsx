d8e69895d491a48016364719978082d200602064
Farzad Wahabzada <faz@jupitr.tech>
2023-10-31 00:41:47 +0000

    playlist

--- components/talent/job/job-view/Videos.tsx
+++ components/talent/job/job-view/Videos.tsx
@@ -6,24 +6,28 @@ import VideoTitle from "./videos/VideoTitle"
 const Videos = () => {
   const { talent_job } = useTalentJobState()
 
-  if (talent_job.company_videos.length) {
+  const videos = talent_job.company_videos.sort((v) => (v.primary ? -1 : 1))
+
+  if (videos.length) {
     return (
-      <div className="flex flex-col gap-5">
-        {talent_job.company_videos.map(
-          (video, index) =>
-            index !== 0 && (
-              <div key={video.id} className="flex flex-col gap-3">
-                <VideoPlayer
-                  src={urlHelper.videoUrl(video.video_url) as string}
-                  poster={urlHelper.videoPosterUrl(video.video_url)}
-                />
-                <VideoTitle
-                  name={video?.user?.name}
-                  title={video?.user?.job_title}
-                />
-              </div>
-            )
-        )}
+      <div className="snap-proximity snap-x flex flex-row overflow-scroll gap-3 pb-1">
+        {videos.map((video) => (
+          <div
+            key={video.id}
+            className={`snap-center shrink-0 ${videos.length > 0 && "w-[90%]"}`}
+          >
+            <div className="py-3">
+              <VideoPlayer
+                src={urlHelper.videoUrl(video.video_url) as string}
+                poster={urlHelper.videoPosterUrl(video.video_url)}
+              />
+            </div>
+            <VideoTitle
+              name={video?.user?.name}
+              title={video?.user?.job_title}
+            />
+          </div>
+        ))}
       </div>
     )
   } else {
