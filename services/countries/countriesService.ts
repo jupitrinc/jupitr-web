7106d8fa84ade4ea496b7a813470a4e5a27ba291
Rafael Magalhaes <rafaelmagalhaes93@gmail.com>
2023-11-01 21:56:01 +0000

    Countries (#187)

--- services/countries/countriesService.ts
+++ services/countries/countriesService.ts
@@ -1,25 +1,33 @@
 import { supabaseClientComponent } from "services/_supabase/client"
-import { getError } from "../_supabase/edgeFunctions"
 
 const countriesService = () => {
-  const searchCountries = async (searchTerm: string) => {
-    const { data, error: err } = await supabaseClientComponent.functions.invoke(
-      `countries?search=${searchTerm}`,
-      {
-        method: "GET",
-      }
-    )
-    if (err) {
-      const error = await getError(err, "searchCountries")
-
+  const searchCountries = async (
+    searchTerm: string,
+    options: { withCity: boolean }
+  ) => {
+    const { data, error } = await supabaseClientComponent
+      .from("countries")
+      .select(`${options?.withCity ? "*, cities(name)" : "*"}`)
+      .textSearch("name", `'${searchTerm}'`)
+    if (error) {
       console.error("countriesService -> searchCountries:", error.message)
-      return { error }
+    }
+    return { data, error }
+  }
+  const searchCities = async (search: string) => {
+    const { data, error } = await supabaseClientComponent
+      .from("cities")
+      .select("*, country:country_id(*)")
+      .textSearch("name", `'${search}'`)
+
+    if (error) {
+      console.error("countriesService -> searchCities:", error.message)
     }
 
-    return { data }
+    return { data, error }
   }
 
-  return { searchCountries }
+  return { searchCountries, searchCities }
 }
 
 export default countriesService
