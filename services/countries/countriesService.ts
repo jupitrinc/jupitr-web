146fb0e0eecbeb1b3e8ea025ada85b464f6ec0de
rafa <rafaelmagalhaes93@gmail.com>
2023-11-01 20:59:12 +0000

    update countriesService

--- services/countries/countriesService.ts
+++ services/countries/countriesService.ts
@@ -1,22 +1,18 @@
 import { supabaseClientComponent } from "services/_supabase/client"
-import { getError } from "../_supabase/edgeFunctions"
 
 const countriesService = () => {
-  const searchCountries = async (searchTerm: string) => {
-    const { data, error: err } = await supabaseClientComponent.functions.invoke(
-      `countries?search=${searchTerm}`,
-      {
-        method: "GET",
-      }
-    )
-    if (err) {
-      const error = await getError(err, "searchCountries")
-
+  const searchCountries = async (
+    searchTerm: string,
+    options: { withCity: boolean }
+  ) => {
+    const { data, error } = await supabaseClientComponent
+      .from("countries")
+      .select(`${options?.withCity ? "*, cities(name)" : "*"}`)
+      .textSearch("name", `'${searchTerm}'`)
+    if (error) {
       console.error("countriesService -> searchCountries:", error.message)
-      return { error }
     }
-
-    return { data }
+    return { data, error }
   }
   const searchCities = async (search: string) => {
     const { data, error } = await supabaseClientComponent
