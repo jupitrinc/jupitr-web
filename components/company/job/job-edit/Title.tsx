0f665495d782164eee0170da811933a0065a9fcd
Ismael Teixeira <ismaelteixeira6@gmail.com>
2023-11-02 15:13:51 +0000

    JUP-414-form-submit-text-input-light (#190)
    Co-authored-by: Ismael Teixeira <ismael@MBP-de-Ismael.Home>
    Co-authored-by: Farzad Wahabzada <faz@jupitr.tech>

--- components/company/job/job-edit/Title.tsx
+++ components/company/job/job-edit/Title.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react"
+import React, { useCallback, useRef } from "react"
 import { TextInput } from "ui-library/form/text-input/TextInput"
 import { useCompanyJobAction } from "state/company_job/useCompanyJobAction"
 import { useCompanyJobState } from "state/company_job/useCompanyJobState"
@@ -8,15 +8,23 @@ const Title = () => {
   const { company_job } = useCompanyJobState()
   const { updateTitle } = useCompanyJobAction()
   const { value, setValue } = useReactiveState("", company_job.title)
+  const inputRef = useRef<HTMLInputElement>(null)
 
-  const update = useCallback(() => {
-    if (value && value !== company_job.title) {
-      updateTitle(company_job.id, value.trim())
-    }
-  }, [value, company_job.id])
+  const update = useCallback(
+    (e: React.FormEvent<HTMLFormElement | HTMLInputElement>) => {
+      e.preventDefault()
+      if (value && value !== company_job.title) {
+        updateTitle(company_job.id, value.trim())
+      }
+      if (inputRef.current) {
+        inputRef.current.blur()
+      }
+    },
+    [value, company_job.id]
+  )
 
   return (
-    <div className="basis-1/3 w-full">
+    <form className="basis-1/3 w-full" onSubmit={update}>
       <TextInput
         placeholder="Job title"
         value={value}
@@ -24,8 +32,9 @@ const Title = () => {
         onBlur={update}
         autoFocus={!company_job.title}
         light
+        ref={inputRef}
       />
-    </div>
+    </form>
   )
 }
 
