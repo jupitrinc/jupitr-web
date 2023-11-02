b5b515e3226acc0a6267f315e22334dc0da52800
Ismael Teixeira <ismael@MBP-de-Ismael.Home>
2023-11-02 14:27:49 +0000

    focusing out if the input element after submission

--- components/company/job/job-edit/Title.tsx
+++ components/company/job/job-edit/Title.tsx
@@ -1,18 +1,14 @@
-import React, { useCallback, useState } from "react"
+import React, { useCallback, useRef } from "react"
 import { TextInput } from "ui-library/form/text-input/TextInput"
 import { useCompanyJobAction } from "state/company_job/useCompanyJobAction"
 import { useCompanyJobState } from "state/company_job/useCompanyJobState"
 import { useReactiveState } from "helper/hooks/useReactiveState"
-import { Text } from "ui-library/text/Text"
-import { Button } from "ui-library/button/Button"
-import { Edit } from "lucide-react"
-import { Label } from "ui-library/form/label/Label"
 
 const Title = () => {
   const { company_job } = useCompanyJobState()
   const { updateTitle } = useCompanyJobAction()
   const { value, setValue } = useReactiveState("", company_job.title)
-  const [editing, setEditing] = useState<boolean>(false)
+  const inputRef = useRef<HTMLInputElement>(null)
 
   const update = useCallback(
     (e: React.FormEvent<HTMLFormElement | HTMLInputElement>) => {
@@ -20,39 +16,26 @@ const Title = () => {
       if (value && value !== company_job.title) {
         updateTitle(company_job.id, value.trim())
       }
-      setEditing(false)
+      if (inputRef.current) {
+        inputRef.current.blur()
+      }
     },
     [value, company_job.id]
   )
 
   return (
     <form className="basis-1/3 w-full ml-3" onSubmit={update}>
-      <Label value="Job title" htmlFor="" />
-      {editing && (
-        <div className="relative">
-          <TextInput
-            placeholder="Job title"
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-            onBlur={update}
-            autoFocus={!company_job.title}
-            light
-          />
-        </div>
-      )}
-
-      {!editing && (
-        <div className="flex flex-row gap-3 items-center">
-          <Text as="span" size="xl2">
-            {value}
-          </Text>
-          <Button
-            icon={<Edit className="h-4 w-4" />}
-            onClick={() => setEditing(true)}
-            variant="text"
-          />
-        </div>
-      )}
+      <div className="relative">
+        <TextInput
+          placeholder="Job title"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          onBlur={update}
+          autoFocus={!company_job.title}
+          light
+          ref={inputRef}
+        />
+      </div>
     </form>
   )
 }
