1738a9d9e5e17f81a0bcf00929389fdbe7244767
Farzad Wahabzada <faz@jupitr.tech>
2023-12-04 01:34:34 +0000

    bug fixed

--- components/talent/profile/Footer.tsx
+++ components/talent/profile/Footer.tsx
@@ -2,29 +2,33 @@ import React from "react"
 import { Rocket } from "lucide-react"
 import { Text } from "ui-library/text/Text"
 import { useRouter } from "next/router"
+import { useUserState } from "state/user/useUserState"
 
 const Footer = () => {
   const router = useRouter()
+  const { isLoggedIn } = useUserState()
 
   const goHome = () => {
     router.push("/")
   }
 
   return (
-    <div className="mt-5 flex flex-row items-center justify-center gap-1">
-      <Text as="span" size="xs">
-        <span className="flex flex-row gap-2">
-          <span>Built with</span>
-          <span
-            className="flex flex-row gap-1 hover:cursor-pointer"
-            onClick={goHome}
-          >
-            <Rocket className="h-4 w-4" />
-            <span className="font-medium">jupitr</span>
+    !isLoggedIn && (
+      <div className="mt-5 flex flex-row items-center justify-center gap-1">
+        <Text as="span" size="xs">
+          <span className="flex flex-row gap-2">
+            <span>Built with</span>
+            <span
+              className="flex flex-row gap-1 hover:cursor-pointer"
+              onClick={goHome}
+            >
+              <Rocket className="h-4 w-4" />
+              <span className="font-medium">jupitr</span>
+            </span>
           </span>
-        </span>
-      </Text>
-    </div>
+        </Text>
+      </div>
+    )
   )
 }
 
