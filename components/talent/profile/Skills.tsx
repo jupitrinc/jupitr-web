1c0efae894169d205e2ec067333cba211725cd01
Faz <faz@jupitr.tech>
2023-12-11 21:07:49 +0000

    tweaks (#214)

--- components/talent/profile/Skills.tsx
+++ components/talent/profile/Skills.tsx
@@ -8,7 +8,7 @@ interface props {
 
 const Skills = ({ skills }: props) => {
   return (
-    <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
+    <div className="grid grid-cols-1 gap-5 sm:grid-cols-2">
       {skills?.map((skill) => <SkillCard key={skill.id} skill={skill} />)}
     </div>
   )
